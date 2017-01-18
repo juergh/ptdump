5026c3fa8b0a14354b9db006633675310a30926c
Juerg Haefliger <juergh@gmail.com>
2017-01-18 11:13:25 +0100

    Add copyright headers
    Signed-off-by: Juerg Haefliger <juergh@gmail.com>

--- ptdump_cli.c
+++ ptdump_cli.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2017 Hewlett Packard Enterprise Development, L.P.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ */
+
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
